d32efa71a51920a96e9656aadc9a073bf7d9d70e
vorasilp <vorasilp@gmail.com>
2025-10-03 23:37:38 +0700

    fix compilation error for kernel 6.17

--- drivers/aic8800/aic8800_fdrv/rwnx_rx.c
+++ drivers/aic8800/aic8800_fdrv/rwnx_rx.c
@@ -2368,7 +2368,11 @@ check_len_update:
         hdr = (struct ieee80211_hdr *)(skb->data + msdu_offset);
         rwnx_vif = rwnx_rx_get_vif(rwnx_hw, hw_rxhdr->flags_vif_idx);
         if (rwnx_vif) {
+            #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 17, 0)
             cfg80211_rx_spurious_frame(rwnx_vif->ndev, hdr->addr2, GFP_ATOMIC);
+            #else
+            cfg80211_rx_spurious_frame(rwnx_vif->ndev, hdr->addr2, UNKNOWN_LINK_ID, GFP_ATOMIC);
+            #endif
         }
         goto end;
     }
@@ -2618,8 +2622,15 @@ check_len_update:
                 }
 
                 if (hw_rxhdr->flags_is_4addr && !rwnx_vif->use_4addr) {
+                    #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 17, 0)
                     cfg80211_rx_unexpected_4addr_frame(rwnx_vif->ndev,
                                                        sta->mac_addr, GFP_ATOMIC);
+                    #else
+                    cfg80211_rx_unexpected_4addr_frame(rwnx_vif->ndev,
+                                                       sta->mac_addr, 
+                                                       UNKNOWN_LINK_ID,
+                                                       GFP_ATOMIC);
+                    #endif
                 }
             }
 
